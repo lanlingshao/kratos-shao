d7a8b1a1677c75ff77d3782c3c0037a522100cf2
shao <lanling_shao@126.com>
2024-11-07 14:05:24 +0800

    add comment

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package kratos
 import (
 	"context"
 	"errors"
+	"fmt"
 	"os"
 	"os/signal"
 	"sync"
@@ -82,6 +83,7 @@ func (a *App) Endpoint() []string {
 
 // Run executes all OnStart hooks registered with the application's Lifecycle.
 func (a *App) Run() error {
+	// 通过 kratos.Server()声明的server实例,并通过 buildInstance() 转换成 *registry.ServiceInstance struct,用于服务注册
 	instance, err := a.buildInstance()
 	if err != nil {
 		return err
@@ -93,39 +95,53 @@ func (a *App) Run() error {
 	eg, ctx := errgroup.WithContext(sctx)
 	wg := sync.WaitGroup{}
 
+	// 执行 helloword/cmd/main.go中的 newApp()中传入的BeforeStart方法中的函数
 	for _, fn := range a.opts.beforeStart {
 		if err = fn(sctx); err != nil {
 			return err
 		}
 	}
+	// 遍历通过 kratos.Server() 声明的服务实例
 	for _, srv := range a.opts.servers {
 		server := srv
+		// 执行两个goroutine, 用于处理服务启动和退出
 		eg.Go(func() error {
-			<-ctx.Done() // wait for stop signal
+			<-ctx.Done() // wait for stop signal // 阻塞wait for stop signal,等待调用 cancel 方法
+			// 如果没有在a.opts.stopTimeout的时间内停掉server的实例，则自动调用cancel方法
 			stopCtx, cancel := context.WithTimeout(NewContext(a.opts.ctx, a), a.opts.stopTimeout)
 			defer cancel()
+			// 协程退出后,调用server实例的停止方法，
+			// httpServer调用go-kratos/kratos/v2/调用transport/http/server.go
+			// grpcServer调用go-kratos/kratos/v2/transport/grpc/server.go
 			return server.Stop(stopCtx)
 		})
+		// 注意:下面需要利用WaitGroup来保证各个server启动后才继续进行下一步
 		wg.Add(1)
 		eg.Go(func() error {
+			fmt.Println("wg done")
+			// 服务启动成功后再进行下面的服务注册
 			wg.Done() // here is to ensure server start has begun running before register, so defer is not needed
+			// 任意一个server启动失败的时候,都会返回err.导致errgroup的ctx被取消
 			return server.Start(NewContext(a.opts.ctx, a))
 		})
 	}
 	wg.Wait()
 	if a.opts.registrar != nil {
+		// 服务注册，如果在registrarTimeout时间内还没有注册成功，则直接调用rcancel
 		rctx, rcancel := context.WithTimeout(ctx, a.opts.registrarTimeout)
 		defer rcancel()
 		if err = a.opts.registrar.Register(rctx, instance); err != nil {
 			return err
 		}
 	}
+	// 执行 helloword/cmd/main.go中的 newApp()中传入的AfterStart方法中的函数
 	for _, fn := range a.opts.afterStart {
 		if err = fn(sctx); err != nil {
 			return err
 		}
 	}
 
+	// 监听进程退出信号,退出信号会发送到c中，只有a.opts.sigs里面的信号才会被监听，a.opts.sigs在New(opts ...Option) *App 中设置
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, a.opts.sigs...)
 	eg.Go(func() error {
@@ -133,6 +149,7 @@ func (a *App) Run() error {
 		case <-ctx.Done():
 			return nil
 		case <-c:
+			// 接收到退出信号，调用Stop方法关闭应用
 			return a.Stop()
 		}
 	})
@@ -140,6 +157,7 @@ func (a *App) Run() error {
 		return err
 	}
 	err = nil
+	// 执行 helloword/cmd/main.go中的 newApp()中传入的 AfterStop 方法中的函数
 	for _, fn := range a.opts.afterStop {
 		err = fn(sctx)
 	}
@@ -149,6 +167,7 @@ func (a *App) Run() error {
 // Stop gracefully stops the application.
 func (a *App) Stop() (err error) {
 	sctx := NewContext(a.ctx, a)
+	// 执行 helloword/cmd/main.go中的 newApp()中传入的 BeforeStop 方法中的函数
 	for _, fn := range a.opts.beforeStop {
 		err = fn(sctx)
 	}
